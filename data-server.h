0b58bb82a9ba0353c46316f15398ce2f9db61d02
jpavieira <vieirajoao94@gmail.om>
2016-05-24 11:44:47 +0100

    adding new header

---
+++ data-server.h
@@ -0,0 +1,26 @@
+#ifndef DATASERVER_H
+#define DATASERVER_H
+
+#include "psiskv.h"
+#include "database.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+
+kv_client2server message_thread;
+
+int read_db(int socket_fd, kv_client2server message);
+int write_db(int socket_fd, kv_client2server message);
+int delete_db(int socket_fd, kv_client2server message);
+int close_db(int);
+void * handle_requests(void* arg);
+#endif
